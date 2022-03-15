8b10166035d79482442e170d7d2c0eba2d45c67c
Danilo <danilo.santossouza@outlook.com>
2022-03-15 18:16:37 -0300

    feat: Implementa model e migration users

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,35 @@
+import { Model } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class User extends Model {
+  public id: number;
+
+  public username: string;
+
+  public role: string;
+
+  public email: string;
+
+  public password: number;
+}
+
+User.init({
+}, {
+  underscored: true,
+  sequelize: db,
+  timestamps: false,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default User;
