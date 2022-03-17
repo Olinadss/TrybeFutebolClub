f016c478eaba976985c4d9d9af37b9831b9d1d44
Danilo <danilo.santossouza@outlook.com>
2022-03-17 17:16:12 -0300

    test: Testa se a classe Users retorna um usuario

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -1,74 +1,71 @@
-import * as sinon from 'sinon';
-import * as chai from 'chai';
-import chaiHttp = require('chai-http');
-import Users from '../database/models/Users';
+// import * as sinon from 'sinon';
+// import * as chai from 'chai';
+// import chaiHttp = require('chai-http');
+// import Users from '../database/models/Users';
 
-import { app } from '../app';
+// import { app } from '../app';
 
-import { Response } from 'superagent';
-import { strictEqual } from 'assert';
+// import { Response } from 'superagent';
+// import { strictEqual } from 'assert';
 
-chai.use(chaiHttp);
+// chai.use(chaiHttp);
 
-const { expect } = chai;
+// const { expect } = chai;
 
-describe('1 - Testa Users Model', () => {
+// describe('1 - Testa Users Model', () => {
 
-  // const users = {
-  //   id: 1,
-  //   username: "Admin",
-  //   role: "admin",
-  //   email: "admin@admin.com"
-  // }
+//   // const users = {
+//   //   id: 1,
+//   //   username: "Admin",
+//   //   role: "admin",
+//   //   email: "admin@admin.com"
+//   // }
 
-  // type teste = {
-  //   id: number
-  //   username: string
-  //   role: string
-  //   email: string
-  // }
+//   // type teste = {
+//   //   id: number
+//   //   username: string
+//   //   role: string
+//   //   email: string
+//   // }
 
-  // before(() => {
-  //   const eventMock = { findOne: sinon.spy() };
-  //   sinon.stub(Users, 'findOne').resolves({ users } as Users);
-  // });
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
+//   // before(() => {
+//   //   const eventMock = { findOne: sinon.spy() };
+//   //   sinon.stub(Users, 'findOne').resolves({ users } as Users);
+//   // });
+//   /**
+//    * Exemplo do uso de stubs com tipos
+//    */
 
-  // let chaiHttpResponse: Response;
+//   // let chaiHttpResponse: Response;
 
-  // before(async () => {
-  //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
-  // });
+//   // before(async () => {
+//   //   sinon
+//   //     .stub(Example, "findOne")
+//   //     .resolves({
+//   //       ...<Seu mock>
+//   //     } as Example);
+//   // });
 
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
+//   // after(()=>{
+//   //   (Example.findOne as sinon.SinonStub).restore();
+//   // })
 
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
+//   // it('...', async () => {
+//   //   chaiHttpResponse = await chai
+//   //      .request(app)
+//   //      ...
 
-  //   expect(...)
-  // });
+//   //   expect(...)
+//   // });
 
-  it('Testa se a classe User existe', () => {
-    const objUsers: Users = new Users();
-    expect(objUsers).to.be.an('object');
-  });
+//   it('Testa se a classe User existe', () => {
+//     const objUsers: Users = new Users();
+//     expect(objUsers).to.be.an('object');
+//   });
 
-  it('Testa se a classe User possui o atributo username', () => {
-    const objUsers: Users = new Users();
-    expect(typeof objUsers.username).to.be.a('string');
-  });
-});
-function username(username: any) {
-  throw new Error('Function not implemented.');
-}
+//   it('Testa se a classe User possui o atributo username', () => {
+//     const objUsers: Users = new Users();
+//     expect(typeof objUsers.username).to.be.a('string');
+//   });
+// });
 
---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,47 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
+import Users from '../database/models/Users';
+import UserService from '../database/service/UserService';
+
+import { app } from '../app';
+
+import { Response } from 'superagent';
+import { strictEqual } from 'assert';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('1 - Testa Users Model', () => {
+
+  const users = {
+    id: 1,
+    username: "Admin",
+    role: "admin",
+    email: "admin@admin.com",
+    password: "123456"
+  }
+
+  before(async () => sinon.stub(Users, 'findOne').resolves(users as Users));
+
+  after(async () => {
+    (Users.findOne as sinon.SinonStub).restore();
+  });
+
+  it('Testa se a classe User existe', () => {
+    const objUsers: Users = new Users();
+    expect(objUsers).to.be.an('object');
+  });
+
+  it('Testa se a classe User possui o atributo username', () => {
+    const objUsers: Users = new Users();
+    expect(typeof objUsers.username).to.be.a('string');
+  });
+
+  it('Testa se a classe retorna um user', async () => {
+    const objUsers = await Users.findOne()
+    expect(objUsers).to.be.a('object');
+  });
+});
+
