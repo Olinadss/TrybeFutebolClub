419932fa3f5b9626dc0aa4e35e3903d757e15598
Danilo <danilo.santossouza@outlook.com>
2022-03-24 15:54:29 -0300

    Requisito 20 resolvido

--- app/backend/src/database/controller/matchsController.ts
+++ app/backend/src/database/controller/matchsController.ts
@@ -3,10 +3,13 @@ import MatchsService from '../service/matchsService';
 import StatusCode from './statusCode';
 
 class MatchsController {
-  static getAll = async (_req: Request, res: Response) => {
-    const listMatchs = await MatchsService.getAll();
-
-    res.status(StatusCode.OK).json(listMatchs);
+  static getAll = async (req: Request, res: Response) => {
+    const { inProgress } = req.query;
+    if (inProgress !== undefined) {
+      const status = inProgress === 'true';
+      const listMatchs = await MatchsService.getAll(status);
+      return res.status(StatusCode.OK).json(listMatchs);
+    }
   };
 }
 
--- app/backend/src/database/service/matchsService.ts
+++ app/backend/src/database/service/matchsService.ts
@@ -2,16 +2,26 @@ import Clubs from '../models/Clubs';
 import Matchs from '../models/Matchs';
 
 class MatchsService {
-  static getAll = async () => {
-    const matchs = await Matchs.findAll({
+  static getAll = async (inProgress?: boolean) => {
+    console.log('dentro do if', inProgress);
+
+    if (inProgress === false || inProgress === undefined) {
+      return Matchs.findAll({
+        include: [{
+          model: Clubs, as: 'homeMatch', attributes: { exclude: ['id'] },
+        }, {
+          model: Clubs, as: 'awayMatch', attributes: { exclude: ['id'] },
+        }],
+      });
+    }
+    return Matchs.findAll({
+      where: { inProgress },
       include: [{
         model: Clubs, as: 'homeMatch', attributes: { exclude: ['id'] },
       }, {
         model: Clubs, as: 'awayMatch', attributes: { exclude: ['id'] },
       }],
     });
-
-    return matchs;
   };
 }
 
